6a667dfc4b798c006ad448f186dbb0375bb364b0
Melikami <meli86akerman@gmail.com>
2021-12-09 18:24:17 +0100

    Commented the code properly

--- script.js
+++ script.js
@@ -8,38 +8,16 @@ class Profile {
         this.age = age;
         this.food = food;
     }
-
-    toString() {
-        return (this.name);
-    }
-
-    name() {
-        return (this.name);
-    }
-
-    diet() {
-        return (this.diet);
-    }
-
-    age() {
-        return (this.age);
-    }
-
-    food() {
-        return (this.food);
-    }
-
-    img() {
-        return (this.img);
-    }
 }
 
+/* arrays for object properties */
 let names = ['Swedish Chef'];
 let diets = ['Omnivore'];
 let ages = ['61'];
 let foods = ['Meatballs'];
 let imgs = ['img/profile pic.jpg']
 
+/* array for objects */
 let profiles = [];
 
 /* for loop to create objects */
@@ -47,31 +25,28 @@ for (let i = 0; i <1; i++) {
     profiles.push(new Profile(names[i], diets[i], ages[i], foods[i]));
 }
 
+/* displays object properties in img and p tags */
 window.addEventListener('load', (event) => {
 
-
+        /* displays object properties in p tag */
         nameDisplay = document.getElementById('nameDisplay');
         nameDisplay.innerHTML = names.toString();
 
+        /* displays object properties in p tag  */
         dietDisplay = document.getElementById('dietDisplay');
         dietDisplay.innerHTML = diets.toString();
 
+        /* displays object properties in p tag  */
         ageDisplay = document.getElementById('ageDisplay');
         ageDisplay.innerHTML = ages.toString();
 
+        /* displays object properties in p tag  */
         foodDisplay = document.getElementById('foodDisplay');
         foodDisplay.innerHTML = foods.toString();
 
+        /* displays object properties in img tag with variable */
         var profilePics = document.getElementById('profilePics');
         console.log(profilePics);
         profilePics.src = imgs.toString();
        
     });
-
-
-
-// window.addEventListener('load', (event) => {
-// displayName = document.getElementById('nameDisplay');
-// displayName.innerHTML = profiles[0].toString();
-// });
-
